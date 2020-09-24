2601fa3d04de0bd6c2656eb132df33ab3c8d1804
Sushant Subhash Chavare <64217279+sushantsc2014@users.noreply.github.com>
2020-09-25 00:15:55 +0530

    new add 99

--- create_table.sql
+++ create_table.sql
@@ -1,5 +1,1 @@
-insert into git_to_postgres values (1);
-insert into git_to_postgres values (2);
-insert into git_to_postgres values (3);
-insert into git_to_postgres values (4);
-insert into git_to_postgres values (5);
+insert into git_to_postgres values (9999999999);
