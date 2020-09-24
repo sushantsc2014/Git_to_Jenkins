7c37374cf0c3cb3ea6e4b040989fba8486de2e01
Sushant Subhash Chavare <64217279+sushantsc2014@users.noreply.github.com>
2020-09-25 00:07:08 +0530

    inserting values

--- create_table.sql
+++ create_table.sql
@@ -1,1 +1,5 @@
-create table git_to_postgres(success int)
+insert into git_to_postgres values (1);
+insert into git_to_postgres values (2);
+insert into git_to_postgres values (3);
+insert into git_to_postgres values (4);
+insert into git_to_postgres values (5);
