99ad2cd3b57be2cad7880c572f413c4b5e69c0cb
Sushant Subhash Chavare <64217279+sushantsc2014@users.noreply.github.com>
2020-09-25 12:20:38 +0530

    Update create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -1,7 +1,3 @@
 create table t1(a int);
-insert into t1 values (1);
-insert into t1 values (2);
-insert into t1 values (3);
-insert into t1 values (4);
-
 create table t2(b int);
+insert into t2 values (4);
